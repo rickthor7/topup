6e386897882ba14e339bd08ee13ec5a988471f91
brurrsyd <ibrahim.rsyd@gmail.com>
2022-11-11 00:15:58 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,33 @@
+const scriptURL = "https://script.google.com/macros/s/AKfycby_utqPLbluf47a0uTAd5e5NBJO1LxRJGEsNLAi8-07fUe-MVcGMGReX9gr4fyrgiGPzg/exec";
+const form = document.forms["cotop"];
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    fetch(scriptURL, { method: "POST", body: new FormData(form) })
+        .then((response) => console.log("Success!", response))
+        .catch((error) => console.error("Error!", error.message));
+        window.location.href = ('./checkout.html');
+    });
+
+async function mlHarga() {
+    try {
+        const response = await fetch("./harga.json");
+        const hargas = await response.json();
+        const listHar = document.getElementById("lihar");
+        hargas.map((a) => {
+            return listHar.innerHTML += (`<div class="col-6">
+            <input type="radio" class="btn-check" name="nominaltopup" id="success-outlined${a.key}" autocomplete="off"  value="${a.key+ ". " + a.diamond + " / " +a.harga}" required/>
+            <label class="btn btn-outline-primary price-box" for="success-outlined${a.key}">
+                <div>
+                    <span>${a.diamond}</span>
+                    <br />
+                    <span>${a.harga}</span>
+                </div>
+            </label>
+        </div>`);
+        });
+    } catch (err) {
+        return err;
+    }
+}
+mlHarga();
